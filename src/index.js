92dbddd474e199fec2fc8b620627aae290ca191e
Jordan <jordandick@gmail.com>
2018-04-12 22:42:48 +0100

    Example 3, Github lookup,

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import {Button} from './Button';
 import {Button2, App2} from './Button2';
+import {GitLookup} from './github-lookup';
+
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Button />, document.getElementById('counter'));
 ReactDOM.render(<App2 />, document.getElementById('app2'));
+ReactDOM.render(<GitLookup/>, document.getElementById('gitLookup'));
 
 registerServiceWorker();
